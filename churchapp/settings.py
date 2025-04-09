ece5467efc47a5197675245d08001971d22c0cc5
YanYan Chan <yly80103@gmail.com>
2025-04-10 01:49:02 +0700

    fix static file

--- churchapp/settings.py
+++ churchapp/settings.py
@@ -131,6 +131,8 @@ LOCALE_PATHS = [
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 STATIC_URL = 'static/'
 
